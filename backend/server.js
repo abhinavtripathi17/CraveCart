328688966c5a02a0eeb481d2f66c0debaed858b6
abhinavtripathi17 <abhinavtripathi1708@gmail.com>
2026-04-13 22:07:41 +0530

    update API URL to production in App and StoreContext components; add additional CORS origin in server configuration

--- backend/server.js
+++ backend/server.js
@@ -14,7 +14,12 @@ const port =process.env.PORT || 4000;
 //middlewares
 app.use(express.json());
 app.use(cors({
-  origin: ["http://localhost:5173", "http://localhost:5174", process.env.FRONTEND_URL].filter(Boolean),
+  origin: [
+    "http://localhost:5173",
+    "http://localhost:5174",
+    "https://cravecraftabhinavtripathi.vercel.app",
+    process.env.FRONTEND_URL
+  ].filter(Boolean),
   credentials: true
 }));
 
